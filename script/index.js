51d6c0b45e3450c767c2546bbfb0bb8d90a79078
Borrel <lborrelli248@example.com>
2025-11-29 11:35:14 +0100

    fea: accueil section

---
+++ script/index.js
@@ -0,0 +1,43 @@
+const phrases = [
+  "Futur Étudiant BUT Informatique",
+  "Passioné par le Developpement",
+  "Bienvenue sur mon Portfolio",
+  "Hello, I'm the Next Developer"
+];
+
+const element = document.getElementById("typewriter");
+
+let phraseIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentPhrase = phrases[phraseIndex];
+
+  if (!isDeleting) {
+    // tape le texte
+    element.textContent = currentPhrase.slice(0, charIndex++);
+  } else {
+    // efface
+    element.textContent = currentPhrase.slice(0, charIndex--);
+  }
+
+  // Quand la phrase est finie → commence à effacer
+  if (charIndex === currentPhrase.length + 1) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1000); // pause avant d'effacer
+    return;
+  }
+
+  // Quand tout est effacé → phrase suivante
+  if (charIndex === 0) {
+    isDeleting = false;
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+  }
+
+  // vitesse d'écriture / effacement
+  const speed = isDeleting ? 50 : 100;
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
